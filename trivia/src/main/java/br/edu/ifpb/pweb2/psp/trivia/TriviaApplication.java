9ca1f25eed6bca2460af5cce3f8ceeaddc85086a
SuetoneCarneiro <suetoneneto12@gmail.com>
2026-05-16 18:21:11 -0300

    Refatora script de teste do banco

--- trivia/src/main/java/br/edu/ifpb/pweb2/psp/trivia/TriviaApplication.java
+++ trivia/src/main/java/br/edu/ifpb/pweb2/psp/trivia/TriviaApplication.java
@@ -1,7 +1,12 @@
 package br.edu.ifpb.pweb2.psp.trivia;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.edu.ifpb.pweb2.psp.trivia.entities.Participante;
+import br.edu.ifpb.pweb2.psp.trivia.repositories.ParticipanteRepository;
 
 @SpringBootApplication
 public class TriviaApplication {
@@ -11,24 +16,35 @@ public class TriviaApplication {
 	}
 
     // Teste para verificar se o banco de dados está funcionando corretamente
-	// @Bean 
-    // public CommandLineRunner testarBanco(ParticipanteRepository repo) {
-    //     return args -> {
-    //         System.out.println(">>> A tentar salvar participante de teste...");
-            
-    //         Participante p = new Participante();
-    //         p.setNome("Pedro Lucas");
-    //         p.setEmail("pepe@gmail.com");
-    //         p.setAdm(true);
-            
-    //         repo.save(p); // O JPA gera o INSERT sozinho
-            
-    //         System.out.println(">>> Sucesso! Participante salvo com ID: " + p.getId());
-            
-    //         // Listar para confirmar
-    //         repo.findAll().forEach(part -> {
-    //             System.out.println(">>> Participante no Banco: " + part.getNome());
-    //         });
-    //     };
-    // }
+	@Bean 
+    public CommandLineRunner testarBanco(ParticipanteRepository repo) {
+        return args -> {
+            // fazendo essa checagem aqui a gnt garante que o banco 
+            // de dados está vazio. Só cria se não existir participante
+            if (repo.count() == 0) {
+                System.out.println(">>> A tentar salvar participante de teste...");
+                
+                Participante p = new Participante();
+                p.setNome("Pedro Lucas");
+                p.setEmail("pepe@gmail.com");
+                p.setAdm(true);
+                
+                repo.save(p); // O JPA gera o INSERT sozinho
+
+                Participante comum = new Participante();
+                comum.setNome("Usuario");
+                comum.setEmail("usuario@trivia.com");
+                comum.setAdm(false);
+
+                repo.save(comum);
+                
+                System.out.println(">>> Sucesso! Participantes salvos no banco de dados");
+                
+                // Listar para confirmar
+                repo.findAll().forEach(part -> {
+                    System.out.println(">>> Participante no Banco: " + part.getNome());
+                });
+            }
+        };
+    }
 }
